5852dbba7e18601e80f8d9ec9a653c8ac2ede0ec
Rajib Podder <rajibpodder12@gmail.com>
2024-12-27 09:16:30 +1100

    convert into canoncal coding style

--- main.tf
+++ main.tf
@@ -1,43 +1,43 @@
 terraform {
-    required_providers {
-      aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.0"
-      }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
 
 provider "aws" {
-    region = var.region
+  region = var.region
 }
 
 data "aws_ami" "amzn2" {
   most_recent = true
-  owners = ["amazon"]
+  owners      = ["amazon"]
   filter {
-    name = "root-device-type"
+    name   = "root-device-type"
     values = ["ebs"]
   }
   filter {
-    name = "virtualization-type"
-    values = [ "hvm" ]
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
   filter {
-    name = "architecture"
+    name   = "architecture"
     values = ["x86_64"]
   }
-   filter {
-   name   = "name"
-   values = ["amzn2-ami-hvm*"]
- }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
 }
 
 resource "aws_instance" "ec2" {
-    ami = data.aws_ami.amzn2.id
-    instance_type = var.instance_type
-    user_data = file("${path.module}/userdata.sh")
-    tags = {
-        "Name" = "ec2"
-        "ManagedBy" = "terraform"
-    }
+  ami           = data.aws_ami.amzn2.id
+  instance_type = var.instance_type
+  user_data     = file("${path.module}/userdata.sh")
+  tags = {
+    "Name"      = "ec2"
+    "ManagedBy" = "terraform"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
-variable "region" { 
-    type = string
-    description = "provide aws_region as input variable"
-    }
+variable "region" {
+  type        = string
+  description = "provide aws_region as input variable"
+}
 variable "instance_type" {
-    type = string
-    description = "instance type should be t2.micro, otherwise it will error out"
-    validation {
-        condition = var.instance_type != "t2.micro"
-        error_message = "Instance type should be t2.micro."
-    }
+  type        = string
+  description = "instance type should be t2.micro, otherwise it will error out"
+  validation {
+    condition     = var.instance_type != "t2.micro"
+    error_message = "Instance type should be t2.micro."
+  }
 }
