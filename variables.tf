39487f461cb6707514c17593988c626073452600
rajibpodder12 <50256557+rajibpodder12@users.noreply.github.com>
2024-12-27 09:15:18 +1100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,6 +7,6 @@ variable "instance_type" {
     description = "instance type should be t2.micro, otherwise it will error out"
     validation {
         condition = var.instance_type != "t2.micro"
-        error_message = "Instance type should be t2.micro"
+        error_message = "Instance type should be t2.micro."
     }
 }
