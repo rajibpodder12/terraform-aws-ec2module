c49732f062a7c80e17c848d0ba4b442daf957c90
Rajib Podder <rajibpodder12@gmail.com>
2024-12-27 08:49:46 +1100

    changing the structure

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,15 @@
 output "ec2_public_ip" {
     value = aws_instance.ec2.public_ip
+    description = "output public ip of ec2 instance"
 }
 
 output "ec2_instance_id" {
     value = aws_instance.ec2.id
+    description = "output ec2 instance id"
 }
 
 
 output "ec2_instance_private_ip" {
     value = aws_instance.ec2.private_ip
+    description = "output ec2 instance private ip"
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,10 @@
-variable "region" { type = string}
+variable "region" { 
+    type = string
+    description = "provide aws_region as input variable"
+    }
 variable "instance_type" {
     type = string
+    description = "instance type should be t2.micro, otherwise it will error out"
     validation {
         condition = var.instance_type != "t2.micro"
         error_message = "Instance type should be t2.micro"
