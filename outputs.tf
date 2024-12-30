d7da3d8984b0b2d3b12f620061ed53906216e586
rajibpodder12 <50256557+rajibpodder12@users.noreply.github.com>
2024-12-30 16:53:28 +1100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,8 +8,22 @@ output "ec2_instance_id" {
   description = "output ec2 instance id"
 }
 
-
 output "ec2_instance_private_ip" {
   value       = aws_instance.ec2.private_ip
   description = "output ec2 instance private ip"
+}
+
+
+output "ami_id" {
+  value = data.aws_ami.ami_id.id
+}
+
+output "own_public_ip" {
+  value = chomp(data.http.ownip.response_body)
+}
+
+output "http_url" {
+  value = <<-EOT
+    httpd url: http://${aws_instance.ec2.public_ip}
+    EOT
 }
